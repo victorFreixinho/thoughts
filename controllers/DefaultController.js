bbb0916aac902d256e389b35514e50f6817a1faa
Victor Freixinho <victorfn01@gmail.com>
2022-08-07 15:28:16 -0300

    WIP

--- controllers/DefaultController.js
+++ controllers/DefaultController.js
@@ -1,6 +1,6 @@
 module.exports = class DefaultController {
   static home(req, res) {
-    res.render("home");
+    res.render("home", { content: { logged: false, thoughts: true } });
   }
   static notFound(req, res) {
     res.render("not-found");
